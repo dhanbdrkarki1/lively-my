ac501b10359ac02a716a2e1f437a09ab2aab0963
Dhan-Bdr-Karki <dhanbdrkarki111@gmail.com>
2024-10-27 14:43:02 +0545

    db updated

--- connection.php
+++ connection.php
@@ -1,14 +1,15 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'livelymydb';
+// # local db
+// $host = 'localhost';
+// $username = 'root';
+// $password = '';
+// $database = 'livelymydb';
 
 // Amazon RDS
-// $host = 'livelymy-instance.czgaq2ekg3ad.us-east-1.rds.amazonaws.com';
-// $username = 'admin';
-// $password = 'Password0123';
-// $database = 'livelymy';
+$host = 'livelymy-instance.czgaq2ekg3ad.us-east-1.rds.amazonaws.com';
+$username = 'admin';
+$password = 'Password0123';
+$database = 'livelymy';
 
 // Create connection
 $database = new mysqli($host, $username, $password, $database);
